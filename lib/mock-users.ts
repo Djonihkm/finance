95de9ca50e5836cd31d1002555517608cf8e7466
Djoni Ouedanou <168178663+Djonihkm@users.noreply.github.com>
2026-04-21 11:19:50 +0100

    Delete lib/mock-users.ts

--- lib/mock-users.ts
+++
@@ -1,6 +0,0 @@
-export type UserRole = "admin" | "etablissement";
-
-export const MOCK_USERS = [
-  { email: "admin@ministere.bj", password: "admin123", role: "admin" as UserRole },
-  { email: "lycee@behan.bj", password: "etab123", role: "etablissement" as UserRole },
-];
